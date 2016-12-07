204bb3ed16d6055ba386fa8a4b34ef3309f4da0a
acomito <arcomito@Anthonys-MacBook-Air-2.local>
2016-12-07 17:02:25 -0500

    more

--- imports/ui/pages/twilio-chat/twilio-chat.js
+++ imports/ui/pages/twilio-chat/twilio-chat.js
@@ -10,8 +10,8 @@ export class TwilioChat extends React.Component {
 
 	}
 	startVideo(){
-		const { token } = this.props;
-		const client = new Video.Client(token, , { logLevel: 'debug' });
+		const token = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiIsImN0eSI6InR3aWxpby1mcGE7dj0xIn0.eyJqdGkiOiJTSzU2ZGFjMDU5ODdhM2NlMzEzMDNhNDhjZDFlZWY1OTNkLTE0ODExNDcwMzciLCJpc3MiOiJTSzU2ZGFjMDU5ODdhM2NlMzEzMDNhNDhjZDFlZWY1OTNkIiwic3ViIjoiQUNkYzJiN2NiNzdhMDVjMThlMDAzODE4OGVmM2I5ZDA5ZCIsImV4cCI6MTQ4MTE1MDYzNywiZ3JhbnRzIjp7ImlkZW50aXR5IjoiYW50aG9ueSIsInJ0YyI6eyJjb25maWd1cmF0aW9uX3Byb2ZpbGVfc2lkIjoiVlNlNWViNTFjMWY0N2JiYTczYjgyYTllZWQ2Y2E2NmZmMCJ9fX0.lPheRPov7gs0VMbk6_GptoLy1loM4k5L3x9Q3WNCPzQ'
+		const client = new Video.Client(token,{ logLevel: 'debug' });
 
 		const localMedia = new Video.LocalMedia();
 
@@ -21,14 +21,13 @@ export class TwilioChat extends React.Component {
 		});
 
 		// Or you can add the microphone and camera independently.
-		localMedia.addMicrophone().then(() => {
-		  return localMedia.addCamera();
-		});
-
-		client.connect({ to: 'my-room', localMedia: localMedia}, function(error, response){
-			console.log(error);
-			console.log(response);
-
+		client.connect({
+		  to: 'my-room',
+		  localMedia: localMedia
+		}).then(room => {
+		  console.log('Connected to the Room "%s"', room.name);
+		}, error => {
+		  console.error('Failed to connect to the Room', error);
 		});
 
 		
